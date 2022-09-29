af6426454f2bc89ee3d397e198f1bee4a69f3096
uvsq21916859 <marculiosage@gmail.com>
2022-09-29 15:29:35 +0200

    ajout de constructeur

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,11 @@
 public class Fraction{
     
+    public int numerateur;
+    public int denominateur;
+    public Fraction(int numerateur, int denominateur){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
    public String toString() {
        return "je suis une fraction";
    }
