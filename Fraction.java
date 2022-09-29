7b19b46d028a47ae1bf0b4a896e88ad9b2caf232
uvsq21916859 <marculiosage@gmail.com>
2022-09-29 15:42:03 +0200

    constructeur la

--- Fraction.java
+++ Fraction.java
@@ -2,11 +2,19 @@ public class Fraction{
     
     public int numerateur;
     public int denominateur;
+
     public Fraction(int numerateur, int denominateur){
         this.numerateur = numerateur;
         this.denominateur = denominateur;
     }
+
+    public Fraction(int numerateur){
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+
+
    public String toString() {
-       return "je suis une fraction";
+       return this.numerateur + "/" + this.denominateur + " mohamed ait dris est une fraction";
    }
 }
